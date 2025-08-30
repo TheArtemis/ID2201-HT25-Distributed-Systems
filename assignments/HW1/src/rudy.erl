2cec0776cf0d8f247971b033d5c851461dd28901
TheArtemis <lordef01@gmail.com>
2025-08-30 16:18:50 +0200

    Working webserver

---
+++ assignments/HW1/src/rudy.erl
@@ -0,0 +1,46 @@
+-module(rudy).
+-export([init/1, request/1]).
+-export([start/1, stop/0]).
+
+init(Port) ->
+    Opt = [list, {active, false}, {reuseaddr, true}],
+    case gen_tcp:listen(Port, Opt) of
+        {ok, Listen} ->
+            handler(Listen),
+            gen_tcp:close(Listen),
+            ok;
+        {error, _} ->
+            error
+    end.
+
+handler(Listen) ->
+    case gen_tcp:accept(Listen) of
+        {ok, Client} ->
+            request(Client),
+            handler(Listen);
+        {error, _} ->
+            error
+    end.
+
+request(Client) ->
+    Recv = gen_tcp:recv(Client, 0),
+    case Recv of
+        {ok, Str} ->
+            {Request, Headers, Body} = http:parse_request(Str),
+            io:format("Received Request: ~p~n", [Request]),
+            io:format("Received Headers: ~p~n", [Headers]),
+            io:format("Received Body: ~p~n", [Body]),
+            Response = reply({Request, Headers, Body}),
+            gen_tcp:send(Client, Response);
+        {error, Error} ->
+            io:format("rudy: error: ~w~n", [Error])
+    end,
+    gen_tcp:close(Client).
+
+reply({{get, URI, _}, _, _}) ->
+    http:ok("You requested the URI: " ++ URI).
+
+start(Port) ->
+    register(rudy, spawn(fun() -> init(Port) end)).
+stop() ->
+    exit(whereis(rudy), "time to die").
