fee2652062c44ed66b207403d0bab07bd8e54363
TheArtemis <lordef01@gmail.com>
2025-09-24 20:15:07 +0200

    Working window

---
+++ assignments/HW4/src/gms1.erl
@@ -0,0 +1,73 @@
+-module(gms1).
+
+-export([leader/4, slave/5, start/1, start/2, init/2, init/3]).
+
+start(Id) ->
+    Self = self(),
+    {ok, spawn_link(fun() -> init(Id, Self) end)}.
+
+init(Id, Master) ->
+    leader(Id, Master, [], [Master]).
+
+start(Id, Grp) ->
+    Self = self(),
+    {ok, spawn_link(fun() -> init(Id, Grp, Self) end)}.
+
+init(Id, Grp, Master) ->
+    Self = self(),
+    Grp ! {join, Master, Self},
+    receive
+        {view, [Leader | Slaves], Group} ->
+            Master ! {view, Group},
+            slave(Id, Master, Leader, Slaves, Group)
+    end.
+
+% Start a worker given:
+% Id: a unique interger, only used for debugging
+% Master: rocess identifier of the application layer
+% Slaves: an ordered list of the process identifiers of all slaves in the group
+% Group: a list of all application layer processes in the group
+
+%                   %WRK   %SLAVES
+leader(Id, Master, Slaves, Group) ->
+    receive
+        {mcast, Msg} ->
+            bcast(Id, {msg, Msg}, Slaves),
+            Master ! Msg,
+            leader(Id, Master, Slaves, Group);
+        % Wrk: Process Identifier of application layer
+        % Peer: Process Identifier of the group process
+        {join, Wrk, Peer} ->
+            Slaves2 = lists:append(Slaves, [Peer]),
+            Group2 = lists:append(Group, [Wrk]),
+            bcast(Id, {view, [self() | Slaves2], Group2}, Slaves2),
+            Master ! {view, Group2},
+            leader(Id, Master, Slaves2, Group2);
+        stop ->
+            ok
+    end.
+
+slave(Id, Master, Leader, Slaves, Group) ->
+    receive
+        % request from its master to multicast a message, the message is forwarded to the leader
+        {mcast, Msg} ->
+            Leader ! {mcast, Msg},
+            slave(Id, Master, Leader, Slaves, Group);
+        % equest from the master to allow a new node to join the group, the message is forwarded to the leader
+        {join, Wrk, Peer} ->
+            Leader ! {join, Wrk, Peer},
+            slave(Id, Master, Leader, Slaves, Group);
+        % multicasted message from the leader. A message Msg is sent to the master
+        {msg, Msg} ->
+            Master ! Msg,
+            slave(Id, Master, Leader, Slaves, Group);
+        % a multicasted view from the leader. A view is delivered to the master process
+        {view, [Leader | Slaves2], Group2} ->
+            Master ! {view, Group2},
+            slave(Id, Master, Leader, Slaves2, Group2);
+        stop ->
+            ok
+    end.
+
+bcast(_Id, Msg, Nodes) ->
+    lists:foreach(fun(Node) -> Node ! Msg end, Nodes).
