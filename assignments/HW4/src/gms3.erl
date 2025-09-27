5d02bc334e73ec674da24954e3784a68af55707a
TheArtemis <lordef01@gmail.com>
2025-09-27 15:23:56 +0200

    Workig gms3

--- assignments/HW4/src/gms3.erl
+++ assignments/HW4/src/gms3.erl
@@ -1,9 +1,13 @@
 -module(gms3).
 
--export([leader/4, slave/5, start/1, start/2, init/3, init/4]).
+-export([leader/5, slave/7, start/1, start/2, init/3, init/4]).
 
 -define(timeout, 2000).
 -define(arghh, 100). % Once every 100
+
+% Reliability:
+% If L -> A & L -> B:
+% if B recv then A recv
 
 start(Id) ->
     Rnd = rand:uniform(1000),
@@ -12,7 +16,7 @@ start(Id) ->
 
 init(Id, Rnd, Master) ->
     rand:seed(default, {Rnd, Rnd, Rnd}),
-    leader(Id, Master, [], [Master]).
+    leader(Id, Master, 1, [], [Master]).
 
 start(Id, Grp) ->
     Self = self(),
@@ -24,9 +28,12 @@ init(Id, Rnd, Grp, Master) ->
     Self = self(),
     Grp ! {join, Master, Self},
     receive
+        {view, N, [Leader | Slaves], Group} ->
+            Master ! {view, Group},
+            slave(Id, Master, Leader, N + 1, {view, N, [Leader | Slaves], Group}, Slaves, Group);
         {view, [Leader | Slaves], Group} ->
             Master ! {view, Group},
-            slave(Id, Master, Leader, Slaves, Group)
+            slave(Id, Master, Leader, 1, undefined, Slaves, Group)
     after ?timeout ->
         Master ! {error, "no reply from leader"}
     end.
@@ -38,59 +45,79 @@ init(Id, Rnd, Grp, Master) ->
 % Group: a list of all application layer processes in the group
 
 %                   %WRK   %SLAVES
-leader(Id, Master, Slaves, Group) ->
+leader(Id, Master, N, Slaves, Group) ->
     receive
         {mcast, Msg} ->
-            bcast(Id, {msg, Msg}, Slaves),
+            bcast(Id, {msg, N, Msg}, Slaves),
             Master ! Msg,
-            leader(Id, Master, Slaves, Group);
+            leader(Id, Master, N + 1, Slaves, Group);
         % Wrk: Process Identifier of application layer
         % Peer: Process Identifier of the group process
         {join, Wrk, Peer} ->
             Slaves2 = lists:append(Slaves, [Peer]),
             Group2 = lists:append(Group, [Wrk]),
-            bcast(Id, {view, [self() | Slaves2], Group2}, Slaves2),
+            bcast(Id, {view, N, [self() | Slaves2], Group2}, Slaves2),
             Master ! {view, Group2},
-            leader(Id, Master, Slaves2, Group2);
+            leader(Id, Master, N + 1, Slaves2, Group2);
         stop ->
             ok
     end.
 
-slave(Id, Master, Leader, Slaves, Group) ->
+slave(Id, Master, Leader, N, Last, Slaves, Group) ->
     erlang:monitor(process, Leader),
     receive
         % request from its master to multicast a message, the message is forwarded to the leader
         {mcast, Msg} ->
             Leader ! {mcast, Msg},
-            slave(Id, Master, Leader, Slaves, Group);
+            slave(Id, Master, Leader, N, Last, Slaves, Group);
         % request from the master to allow a new node to join the group, the message is forwarded to the leader
         {join, Wrk, Peer} ->
             Leader ! {join, Wrk, Peer},
-            slave(Id, Master, Leader, Slaves, Group);
+            slave(Id, Master, Leader, N, Last, Slaves, Group);
+        % Case of a old message
+        {msg, I, _} when I < N ->
+            io:format("slave ~w: ignoring message with seq ~w, current ~w~n", [Id, I, N]),
+            slave(Id, Master, Leader, N, Last, Slaves, Group);
+        % Case of a old view message
+        {view, I, _, _} when I < N ->
+            io:format("slave ~w: ignoring message with seq ~w, current ~w~n", [Id, I, N]),
+            slave(Id, Master, Leader, N, Last, Slaves, Group);
         % multicasted message from the leader. A message Msg is sent to the master
-        {msg, Msg} ->
+        {msg, N, Msg} -> % N: sequence number
             Master ! Msg,
-            slave(Id, Master, Leader, Slaves, Group);
+            slave(Id, Master, Leader, N + 1, {msg, N, Msg}, Slaves, Group);
         % a multicasted view from the leader. A view is delivered to the master process
-        {view, [Leader | Slaves2], Group2} ->
+        {view, N, [Leader | Slaves2], Group2} ->
             Master ! {view, Group2},
-            slave(Id, Master, Leader, Slaves2, Group2);
+            slave(Id,
+                  Master,
+                  Leader,
+                  N + 1,
+                  {view, N, [Leader | Slaves2], Group2},
+                  Slaves2,
+                  Group2);
         {'DOWN', _Ref, process, Leader, _Reason} ->
-            election(Id, Master, Slaves, Group);
+            election(Id, Master, N, Last, Slaves, Group);
         stop ->
             ok
     end.
 
-election(Id, Master, Slaves, [_ | Group]) ->
+election(Id, Master, N, Last, Slaves, [_ | Group]) ->
     Self = self(),
     case Slaves of
         [Self | Rest] ->
-            bcast(Id, {view, Slaves, Group}, Rest),
+            case Last of
+                undefined ->
+                    ok;
+                _ ->
+                    bcast(Id, Last, Rest)
+            end,
+            bcast(Id, {view, N, Slaves, Group}, Rest),
             Master ! {view, Group},
-            leader(Id, Master, Rest, Group);
+            leader(Id, Master, N + 1, Rest, Group);
         [Leader | Rest] ->
             erlang:monitor(process, Leader),
-            slave(Id, Master, Leader, Rest, Group)
+            slave(Id, Master, Leader, N, Last, Rest, Group)
     end.
 
 bcast(Id, Msg, Nodes) ->
--- assignments/HW4/test/commands.md
+++ assignments/HW4/test/commands.md
@@ -4,7 +4,7 @@ W1 = test:first(1, gms1, 1000).
 
 test:add(2, gms1, W1, 1000).
 
-# GMS2 -
+# GMS2
 
 W1 = test:first(1, gms2, 1000).
 
@@ -14,17 +14,56 @@ test:add(3, gms2, W1, 1000).
 
 **Kill the leader**
 
-test:add(10, gms2, W2, 1000).
+test:add(12, gms2, W10, 1000).
 
-# GMS2p5 -
+# GMS2p5
 
 W1 = test:first(1, gms2p5, 1000).
 
 W2 = test:add(2, gms2p5, W1, 1000).
 
-test:add(3, gms2p5, W1, 1000).
+W3 = test:add(3, gms2p5, W2, 1000).
 
+W4 = test:add(4, gms2p5, W3, 1000).
 
+W5 = test:add(5, gms2p5, W4, 1000).
 
+W6 = test:add(6, gms2p5, W5, 1000).
 
-# GMS3 -
+W7 = test:add(7, gms2p5, W6, 1000).
+
+W8 = test:add(8, gms2p5, W7, 1000).
+
+W9 = test:add(9, gms2p5, W8, 1000).
+
+W10 = test:add(10, gms2p5, W9, 1000).
+
+W11 = test:add(11, gms2p5, W10, 1000).
+
+test:add(12, gms2p5, W11, 1000).
+
+# GMS3
+
+W1 = test:first(1, gms3, 1000).
+
+W2 = test:add(2, gms3, W1, 1000).
+
+W3 = test:add(3, gms3, W2, 1000).
+
+W4 = test:add(4, gms3, W3, 1000).
+
+W5 = test:add(5, gms3, W4, 1000).
+
+W6 = test:add(6, gms3, W5, 1000).
+
+W7 = test:add(7, gms3, W6, 1000).
+
+W8 = test:add(8, gms3, W7, 1000).
+
+W9 = test:add(9, gms3, W8, 1000).
+
+W10 = test:add(10, gms3, W9, 1000).
+
+W11 = test:add(11, gms3, W10, 1000).
+
+test:add(12, gms3, W11, 1000).
