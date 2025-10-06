536f1779ffefc582d9668a857547c3f44d4d0cf4
TheArtemis <lordef01@gmail.com>
2025-10-06 23:05:20 +0200

    Storage module

---
+++ assignments/HW5/src/storage.erl
@@ -0,0 +1,27 @@
+-module(storage).
+
+-export([create/0, add/3, lookup/2, split/3, merge/2]).
+
+%% Store is a list of tuple {Key, Value}
+
+create() ->
+    [].
+
+add(Key, Value, Store) ->
+    [{Key, Value} | Store].
+
+lookup(Key, Store) ->
+    lists:keyfind(Key, 1, Store).
+
+split(From, To, Store) ->
+    lists:foldl(fun({Key, Value}, {Updated, Rest}) ->
+                   case key:between(Key, From, To) of
+                       false -> {Updated, [{Key, Value} | Rest]};
+                       true -> {[{Key, Value} | Updated], Rest}
+                   end
+                end,
+                {[], []},
+                Store).
+
+merge(Entries, Store) ->
+    lists:foldl(fun({Key, Value}, Acc) -> add(Key, Value, Acc) end, Store, Entries).
